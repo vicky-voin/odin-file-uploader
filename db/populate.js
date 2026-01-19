6201723b9c3347c31ff0e48955df847d6901f321
vicky-voin <alteneria127@gmail.com>
2026-01-19 15:44:49 -0500

    Installed packages, initialized prisma

---
+++ db/populate.js
@@ -0,0 +1,25 @@
+import { prisma } from "../lib/prisma.js";
+
+async function populateWithTestData() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@test.com",
+    },
+  });
+
+  console.log("Created user: ", user);
+
+  const allUsers = await prisma.user.findMany();
+  console.log("All users: ", JSON.stringify(allUsers, null, 2));
+}
+
+populateWithTestData()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
